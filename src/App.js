71554e473962b524c853a3b992f7816314b6e431
Nimai Kraus <nimai.kraus@gmail.com>
2022-12-06 23:14:26 +0100

    setup iniziale con installazione packages, creazione dei vari components, tailwing e config di craco

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { FiSettings } from "react-icons/fi";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Navbar,
+  Footer,
+  Sidebar,
+  ThemeSettings,
+} from "./components/components_index";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages/pages_index";
 function App() {
+  const isMenuActive = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="relative flex dark:bg-main-dark-bg">
+          <div className="fixed bottom-4 right-4" style={{ zIndex: 1000 }}>
+            <TooltipComponent content={"Settings"}>
+              <button
+                className="button text-3xl text-white hover:bg-black p-3"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {isMenuActive && (
+            <div className="w-72 bg-black min-h-screen dark:bg-main-dark-bg fixed sidebar text-white">
+              <div className="navbar md:static w-full bg-main-bg dark:bg-main-dark-bg fixed">
+                <Navbar />
+              </div>
+              <div>
+                <Routes>
+                  {/* dashboard  */}
+                  <Route path="/" element={(<Ecommerce />)} />
+                  <Route path="/ecommerce" element={(<Ecommerce />)} />
+
+                  {/* pages  */}
+                  <Route path="/orders" element={<Orders />} />
+                  <Route path="/employees" element={<Employees />} />
+                  <Route path="/customers" element={<Customers />} />
+
+                  {/* apps  */}
+                  <Route path="/kanban" element={<Kanban />} />
+                  <Route path="/editor" element={<Editor />} />
+                  <Route path="/calendar" element={<Calendar />} />
+                  <Route path="/color-picker" element={<ColorPicker />} />
+
+                  {/* charts  */}
+                  <Route path="/line" element={<Line />} />
+                  <Route path="/area" element={<Area />} />
+                  <Route path="/bar" element={<Bar />} />
+                  <Route path="/pie" element={<Pie />} />
+                  <Route path="/financial" element={<Financial />} />
+                  <Route path="/color-mapping" element={<ColorMapping />} />
+                  <Route path="/pyramid" element={<Pyramid />} />
+                  <Route path="/stacked" element={<Stacked />} />
+                </Routes>
+              </div>
+            </div>
+          )}
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
